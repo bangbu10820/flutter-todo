09539ca33f3b8cdd4581e2b8a974756152ef376d
Anya Quote <anya.quote@gmail.com>
2023-12-20 09:43:24 +0700

    feat(router.dart): create a file contains router of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/pages/home.dart';
+import 'package:flutter_todo/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: FlexThemeData.light(scheme: FlexScheme.barossa),
-      home: const MyHomePage(),
+      routerConfig: router,
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/pages/home.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class ScreenPaths {
+  static String home = '/';
+  static String create = '/create';
+}
+
+/// The route configuration.
+final GoRouter router = GoRouter(
+  initialLocation: ScreenPaths.home,
+  routes: <RouteBase>[
+    GoRoute(
+      path: ScreenPaths.home,
+      builder: (BuildContext context, GoRouterState state) {
+        return const MyHomePage();
+      },
+    ),
+    GoRoute(
+      path: ScreenPaths.create,
+      builder: (BuildContext context, GoRouterState state) {
+        return const Placeholder();
+      },
+    ),
+  ],
+);
