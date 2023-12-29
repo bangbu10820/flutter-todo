f02fac75734dcf211c9eac50f381d0778aabec38
namngo8 <106657445+namngo8@users.noreply.github.com>
2023-12-29 10:22:23 +0700

    Create 'add task' UI page (#16)
    * create widget
    
    * remove background color
    
    * change create page
    
    * update
    
    * update
    
    * remove maxLines
    
    * quay xe

---
+++ lib/pages/create.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/models/todo.dart';
+import 'package:flutter_todo/provider_models/todo_list_model.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+class CreateToDoPage extends StatelessWidget {
+  const CreateToDoPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController contentController = TextEditingController();
+    var list = context.read<TodoListModel>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: titleController,
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: contentController,
+                minLines: 5,
+                maxLines: null,
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                  alignLabelWithHint: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SizedBox(
+          width: double.infinity,
+          child: FloatingActionButton(
+            onPressed: () {
+              list.add(Todo(
+                  title: titleController.text,
+                  descriptions: contentController.text));
+              context.pop();
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: const Text('Create'),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/provider_models/todo_list_model.dart';
+import 'package:flutter_todo/router.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -7,27 +9,26 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => TodoListModel(),
-      builder: (context, child) => Scaffold(
-        appBar: AppBar(title: const Text("Home Page")),
-        body: Consumer<TodoListModel>(
-          builder: (context, list, child) {
-            return ListView.builder(
-              itemBuilder: (context, index) => ListTile(
-                title: Text(list.todos[index].title),
-                subtitle: Text(list.todos[index].descriptions),
-              ),
-              itemCount: list.todos.length,
-            );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home Page")),
+      body: Consumer<TodoListModel>(
+        builder: (context, list, child) {
+          return ListView.builder(
+            itemBuilder: (context, index) => ListTile(
+              title: Text(list.todos[index].title),
+              subtitle: Text(list.todos[index].descriptions),
+            ),
+            itemCount: list.todos.length,
+          );
+        },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.push(ScreenPaths.create);
+        },
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
